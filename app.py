f77d7fc241d144606543ab2a54468c47fc2b23a6
AbdulRehman-99 <sihamakhan186@gmail.com>
2024-12-28 21:36:50 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# Project:1 Mad libs
+
+name = input("Your name: ")
+adj = input("Adjective: ")
+verb = input("Verb: ")
+fav_num = input("Favourite number: ")
+
+madlibs = f"My name is {name}. Computer programming is so {adj}. My favourite activity is {verb}. My favourite number is {fav_num}"
+
+print(madlibs)
